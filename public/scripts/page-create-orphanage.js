1293b7088645205da3c040e2a60a3635f8ab13ba
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-10-19 20:19:11 -0300

    Salvando coordenadas

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -17,6 +17,9 @@ map.on('click', (event) => {
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
     //remove icon
     marker && map.removeLayer(marker)
 
