69fda3eaff476ae622beddd33f5521a049f0ba6b
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-10-27 17:10:23 -0300

    Criando saveOrphanage

--- src/database/test.js
+++ src/database/test.js
@@ -20,7 +20,7 @@ Database.then(async db => {
            "-38.5694192",
            "Lar dos meninos",
            "Presta assitência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social",
-           "Presta assitência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social",
+           "(85) 99999-8888",
            "https://images.unsplash.com/photo-1576025773492-cc2eb828c42a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
            "Venha como se sentir a vontade e traga muito amor e paciência para dar",
            "Horário de visitas das 8h até ás 18h",
@@ -31,5 +31,9 @@ Database.then(async db => {
     //consultar dados na tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
+
+    //consultar somente um orfanato pelo id  
+    const orphanage = await db.all('Select * from orphanages where id = "1"')
+    console.log(orphanage)
 }
 )
