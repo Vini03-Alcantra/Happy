9ad681674860d5f451eb8b4a8a5a043c5b50976d
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-10-28 00:57:29 -0300

    Finalizando estilo do orphanage

--- src/pages.js
+++ src/pages.js
@@ -12,7 +12,17 @@ module.exports = {
         try{
             const db = await Database;
             const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)            
-            const orphanage = results[0]
+            const orphanage = results[0];
+
+            orphanage.images = orphanage.images.split(',');
+            orphanage.firstImage = orphanage.images[0]
+
+            if (orphanage.open_on_weekends == "0") {
+                orphanage.open_on_weekends = false
+            } else {
+                orphanage.open_on_weekends = true
+            }
+
             return res.render('orphanage', {orphanage})
         } catch(error){
             console.log(error);
