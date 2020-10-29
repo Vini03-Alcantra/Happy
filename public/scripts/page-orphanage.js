523c75d9e6d96a3070320889f1b596049e617a2b
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-10-29 15:27:44 -0300

    Atualizando os pontos fixos do orphanage no mapa

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
     zoomControl: false
 }
 
+// create and add marker 
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
 // create map
-const map = L.map('mapid', options).setView([-3.7581102,-38.5694252], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -20,9 +24,7 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-
-
-L.marker([-3.7581102,-38.5694252], {icon})
+L.marker([lat, lng], {icon})
     .addTo(map)
 
 
--- src/database/test.js
+++ src/database/test.js
@@ -5,19 +5,19 @@ Database.then(async db => {
     //Inserir dados na tabela
 
     await saveOrphanage(db, {        
-        lat:"-3.7121540",
-        lng:"-38.5982291",
-        name:"Lar da Paz",
+        lat:"-3.7125740",
+        lng:"-38.5986291",
+        name:"Lar dos Escolhidos",
         about:"Presta assitência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social",        
-        whatsapp: "(89)90943-5695",
+        whatsapp: "(89)90943-5395",
         images:[
             "https://images.unsplash.com/photo-1576025773492-cc2eb828c42a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
             "https://images.unsplash.com/photo-1602958169883-016fdb9458dc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
         ].toString(),
         instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar",
-        opening_hours:"Horário de visitas das 8h até ás 18h",
-        open_on_weekends: "1"     
+        opening_hours:"Horário de visitas das 8h até ás 20h",
+        open_on_weekends: "0"     
     });
 
     //consultar dados na tabela
